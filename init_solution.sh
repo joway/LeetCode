916c5d58acbc29681e38df0ebd0164d2b3730026
Joway Wang <joway.w@gmail.com>
2017-01-18 23:47:03 +0800

    update

--- init_solution.sh
+++ init_solution.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 if [ $# -eq 2 ]
 then
-  filename=$2/$1.py
+  filename=$1/$2.py
 else
   filename=$1.py
 fi
-sed 's/{{ method }}/'$1'/' code_temp.txt >> $filename
+sed 's/{{ method }}/'$2'/' code_temp.txt >> $filename
+
